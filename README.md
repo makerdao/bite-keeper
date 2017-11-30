dd29ce5f92eea49012761c0abfd601eb12823a0c
reverendus <reverendus@invalid>
2017-11-30 22:10:29 +0100

    Leaving only the `bite-keeper` keeper after copying the whole `keeper` repo

---
+++ README.md
@@ -0,0 +1,14 @@
+# bite-keeper
+
+[![Build Status](https://travis-ci.org/makerdao/bite-keeper.svg?branch=master)](https://travis-ci.org/makerdao/keeper)
+[![codecov](https://codecov.io/gh/makerdao/bite-keeper/branch/master/graph/badge.svg)](https://codecov.io/gh/makerdao/keeper)
+[![Code Climate](https://codeclimate.com/github/makerdao/bite-keeper/badges/gpa.svg)](https://codeclimate.com/github/makerdao/keeper)
+[![Issue Count](https://codeclimate.com/github/makerdao/bite-keeper/badges/issue_count.svg)](https://codeclimate.com/github/makerdao/keeper)
+
+<https://chat.makerdao.com/channel/keeper>
+
+#TODO
+
+## License
+
+See [COPYING](https://github.com/makerdao/keeper/blob/master/COPYING) file.
---
+++ tests/test_bite_keeper.py
@@ -0,0 +1,82 @@
+# This file is part of Maker Keeper Framework.
+#
+# Copyright (C) 2017 reverendus
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import pytest
+
+from bite_keeper import Wad
+from pymaker.feed import DSValue
+from bite_keeper.sai_bite import SaiBite
+from tests.conftest import SaiDeployment
+from tests.helper import args, captured_output
+
+
+class TestBiteKeeper:
+    def test_should_not_start_without_eth_from_argument(self, sai: SaiDeployment):
+        # when
+        with captured_output() as (out, err):
+            with pytest.raises(SystemExit):
+                SaiBite(args=args(f""), web3=sai.web3)
+
+        # then
+        assert "error: the following arguments are required: --eth-from" in err.getvalue()
+
+    def test_should_not_start_without_tub_address_argument(self, sai: SaiDeployment):
+        # when
+        with captured_output() as (out, err):
+            with pytest.raises(SystemExit):
+                SaiBite(args=args(f"--eth-from {sai.web3.eth.defaultAccount}"), web3=sai.web3)
+
+        # then
+        assert "error: the following arguments are required: --tub-address" in err.getvalue()
+
+    def test_should_bite_unsafe_cups_only(self, sai: SaiDeployment):
+        # given
+        keeper = SaiBite(args=args(f"--eth-from {sai.web3.eth.defaultAccount} --tub-address {sai.tub.address}"),
+                         web3=sai.web3)
+
+        # and
+        sai.tub.join(Wad.from_number(10)).transact()
+        sai.tub.cork(Wad.from_number(100000)).transact()
+        DSValue(web3=sai.web3, address=sai.tub.pip()).poke_with_int(Wad.from_number(250).value).transact()
+
+        # and
+        sai.tub.open().transact()
+        sai.tub.lock(1, Wad.from_number(4)).transact()
+        sai.tub.draw(1, Wad.from_number(1000)).transact()
+
+        # and
+        assert sai.tub.safe(1)
+
+        # when
+        keeper.check_all_cups()
+
+        # then
+        assert sai.tub.safe(1)
+        assert sai.tub.tab(1) == Wad.from_number(1000)
+
+        # when
+        DSValue(web3=sai.web3, address=sai.tub.pip()).poke_with_int(Wad.from_number(150).value).transact()
+
+        # and
+        assert not sai.tub.safe(1)
+
+        # and
+        keeper.check_all_cups()
+
+        # then
+        assert sai.tub.safe(1)
+        assert sai.tub.tab(1) == Wad.from_number(0)
